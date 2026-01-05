3ff4f32969f80895366575847b59ebc00e83678b
Halawania <abdu.halwani@gmail.com>
2026-01-05 15:26:58 +0300

    Adjust delta motor direction defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -457,9 +457,15 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#if ENABLED(DELTA)
+  #define INVERT_X_DIR false
+  #define INVERT_Y_DIR false
+  #define INVERT_Z_DIR false
+#else
+  #define INVERT_X_DIR false
+  #define INVERT_Y_DIR true
+  #define INVERT_Z_DIR false
+#endif
 
 // @section extruder
 
