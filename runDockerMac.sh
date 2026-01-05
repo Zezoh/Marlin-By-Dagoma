249e032d821f0532c99ce173f5fb4e6e1760c086
Halawania <abdu.halwani@gmail.com>
2026-01-05 13:05:28 +0300

    Remove non-Arduino build files

--- runDockerMac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pwd=$(pwd)
-echo ""
-echo "********************"
-echo "go to /opt/dist"
-echo "********************"
-echo ""
-docker run --name dockerMarlin -a stdin -a stdout -v $pwd:/opt/dist -i -t ubuntu /bin/bash
-docker rm dockerMarlin
