a8519bae7d1963743144fa08e5514e2212f2ada6
Halawania <abdu.halwani@gmail.com>
2026-07-08 13:30:56 +0300

    Beautify Marlin main source

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -435,7 +435,7 @@
 
 //The ASCII buffer for receiving from the serial:
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 10
+#define BUFSIZE 4
 
 // Bad Serial-connections can miss a received command by sending an 'ok'
 // Therefore some clients abort after 30 seconds in a timeout.
